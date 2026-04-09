79e5dc14bc674f08c798626f0065066be11c3aa3
Zambrinn <zambrinfilho@gmail.com>
2026-04-09 19:03:17 -0300

    feat: initialize project structure with Maven and Spring Boot configuration

---
+++ src/main/kotlin/com/mcpratapp/McpratappApplication.kt
@@ -0,0 +1,11 @@
+package com.mcpratapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class McpratappApplication
+
+fun main(args: Array<String>) {
+	runApplication<McpratappApplication>(*args)
+}
