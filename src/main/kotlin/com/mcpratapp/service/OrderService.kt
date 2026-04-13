0c376e8620da38cdd23f013585921f003fc0496c
Zambrinn <zambrinfilho@gmail.com>
2026-04-13 18:00:10 -0300

    feat: enhance OrderService with order confirmation and payment processing methods

--- src/main/kotlin/com/mcpratapp/service/OrderService.kt
+++ src/main/kotlin/com/mcpratapp/service/OrderService.kt
@@ -1,11 +1,15 @@
 package com.mcpratapp.service
 
+import com.mcpratapp.dto.request.ConfirmOrderRequest
 import com.mcpratapp.dto.response.OrderItemResponse
 import com.mcpratapp.dto.response.OrderResponse
+import com.mcpratapp.dto.response.PaymentResponse
 import com.mcpratapp.model.Client
 import com.mcpratapp.model.Order
 import com.mcpratapp.model.OrderItem
 import com.mcpratapp.model.OrderStatus
+import com.mcpratapp.model.Payment
+import com.mcpratapp.model.PaymentStatus
 import com.mcpratapp.repository.ClientRepository
 import com.mcpratapp.repository.OrderRepository
 import com.mcpratapp.repository.PaymentRepository
@@ -13,6 +17,7 @@ import com.mcpratapp.repository.ProductRepository
 import com.mcpratapp.repository.ProductVendorRepository
 import com.mcpratapp.repository.UserRepository
 import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.data.spel.spi.ExtensionIdAware
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
@@ -42,7 +47,7 @@ class OrderService (
         )
 
         val createdEmptyOrder = orderRepository.save(orderToSave)
-        return convertToDto(createdEmptyOrder)
+        return createdEmptyOrder.toResponse()
     }
 
     fun addItemToOrder(orderId: UUID, productId: UUID, quantity: Int): OrderResponse {
@@ -62,7 +67,7 @@ class OrderService (
         }
 
         if (existingOrder.status != OrderStatus.PENDING) {
-            throw IllegalArgumentException("O status da venda ao ser criado tem que ser pendente.")
+            throw IllegalArgumentException("O status do pedido ao ser criado tem que ser pendente.")
         }
 
         val orderItem = OrderItem(
@@ -77,18 +82,75 @@ class OrderService (
         existingProduct.reservedQuantity += quantity
 
         val updatedOrder = orderRepository.save(existingOrder)
-        return convertToDto(updatedOrder)
+        return updatedOrder.toResponse()
     }
 
-    fun confirmOrder(orderId: UUID): OrderResponse {
-        val existingOrder = orderRepository.findById(orderId)
+    fun confirmOrder(request: ConfirmOrderRequest): OrderResponse {
+        val existingOrder = orderRepository.findById(request.orderId)
             .orElseThrow { IllegalArgumentException("Pedido não encontrado.") }
 
         if (existingOrder.status != OrderStatus.PENDING) {
-            throw IllegalArgumentException("O status da venda ao ser criado tem que ser pendente.")
+            throw IllegalArgumentException("O status do pedido ao ser criado tem que ser pendente.")
         }
 
+        val payment = Payment(
+            order = existingOrder,
+            method = request.paymentMethod,
+            amount = existingOrder.totalAmount,
+        )
+        paymentRepository.save(payment)
 
+        existingOrder.status = OrderStatus.CONFIRMED
+        existingOrder.confirmedAt = LocalDateTime.now()
+        val orderToSave = orderRepository.save(existingOrder)
+        return orderToSave.toResponse()
+    }
+
+    fun confirmPayment(orderId: UUID, paymentId: UUID): OrderResponse {
+        val existingOrder = orderRepository.findById(orderId)
+            .orElseThrow { IllegalArgumentException("Pedido não encontrado.") }
+
+        val payment = paymentRepository.findById(paymentId)
+            .orElseThrow { IllegalArgumentException("Pagamento não encontrado.")  }
+
+        if (payment.order.id != existingOrder.id) {
+            throw IllegalArgumentException("O pagamento ${payment.order.id} não pertence ao pedido ${existingOrder.id}")
+        }
+
+        if (existingOrder.status != OrderStatus.CONFIRMED) {
+            throw IllegalArgumentException("O status do pedido deveria estar confirmado.")
+        }
+
+        if (payment.status != PaymentStatus.PENDING) {
+            throw IllegalArgumentException("O pagamento já foi processado.")
+        }
+
+        existingOrder.items.forEach { item ->
+            item.product.apply {
+                totalQuantity -= item.quantity
+                reservedQuantity -= item.quantity
+            }
+        }
+
+        payment.status = PaymentStatus.PAID
+        payment.paidAt = LocalDateTime.now()
+        existingOrder.status = OrderStatus.COMPLETED
+
+        paymentRepository.save(payment)
+        productRepository.saveAll(existingOrder.items.map { it.product })
+        orderRepository.save(existingOrder)
+
+        return existingOrder.toResponse()
+    }
+
+    fun getOrders(): List<OrderResponse> {
+        val orders: List<Order> = orderRepository.findAll()
+        return orders.map { it.toResponse() }
+    }
+
+    fun getOrderByID(orderId: UUID): OrderResponse? {
+        val foundOrder = orderRepository.findByIdOrNull(orderId)
+        return foundOrder?.toResponse()
     }
 
     private fun convertOrderItemToDto(item: OrderItem): OrderItemResponse {
@@ -101,16 +163,16 @@ class OrderService (
         )
     }
 
-    private fun convertToDto(order: Order): OrderResponse {
+    private fun Order.toResponse(): OrderResponse {
         return OrderResponse(
-            id = order.id,
-            clientId = order.client.id,
-            vendorId = order.vendor.id,
-            status = order.status,
-            totalAmount = order.totalAmount,
-            items = order.items.map { convertOrderItemToDto(it) },
-            createdAt = order.createdAt,
-            confirmedAt = order.confirmedAt
+            id = this.id!!,
+            clientId = this.client.id,
+            vendorId = this.vendor.id,
+            status = this.status,
+            totalAmount = this.totalAmount,
+            items = this.items.map { convertOrderItemToDto(it) },
+            createdAt = this.createdAt,
+            confirmedAt = this.confirmedAt
         )
     }
 }
