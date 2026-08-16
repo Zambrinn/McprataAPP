4c9d945fac4b000ba23f72eb5215c00fb61e2b7b
Zambrinn <zambrinfilho@gmail.com>
2026-08-16 10:52:14 -0300

    feat: implement logic in refreshtoken

--- src/main/kotlin/com/mcpratapp/model/RefreshToken.kt
+++ src/main/kotlin/com/mcpratapp/model/RefreshToken.kt
@@ -1,23 +1,27 @@
 package com.mcpratapp.model
 
 import jakarta.persistence.Column
+import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
 import java.util.UUID
 import java.time.Instant
 
+@Entity
+@Table(name = "refresh_tokens")
 class RefreshToken(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     val id: UUID? = null,
     @Column(nullable = false, unique = true)
-    val token: String,
+    var token: String,
     @OneToOne
     @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
     val user: User,
     @Column(nullable = false)
-    val expiryDate: Instant
+    var expiryDate: Instant
 )
--- src/main/kotlin/com/mcpratapp/service/RefreshTokenService.kt
+++ src/main/kotlin/com/mcpratapp/service/RefreshTokenService.kt
@@ -7,9 +7,11 @@ import com.mcpratapp.repository.RefreshTokenRepository
 import com.mcpratapp.repository.UserRepository
 import jakarta.transaction.Transactional
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Service
 import java.util.UUID
 import java.time.Instant
 
+@Service
 class RefreshTokenService(
     private val refreshTokenRepository: RefreshTokenRepository,
     private val userRepository: UserRepository,
@@ -18,12 +20,21 @@ class RefreshTokenService(
 ) {
     @Transactional
     fun createRefreshToken(user: User): RefreshToken {
-        refreshTokenRepository.deleteByUser(user) // Remove token antigo do usuário
-        val refreshToken = RefreshToken(
-            token = UUID.randomUUID().toString(),
-            user = user,
-            expiryDate = Instant.now().plusMillis(expirationInMs)
-        )
+        val existingToken = refreshTokenRepository.findByUser(user).orElse(null)
+
+        val refreshToken = if (existingToken != null) {
+            existingToken.apply {
+                this.token = UUID.randomUUID().toString()
+                this.expiryDate = Instant.now().plusMillis(expirationInMs)
+            }
+        } else {
+            RefreshToken(
+                token = UUID.randomUUID().toString(),
+                user = user,
+                expiryDate = Instant.now().plusMillis(expirationInMs)
+            )
+        }
+
         return refreshTokenRepository.save(refreshToken)
     }
 
